58dc0daa61e594884a7ea70baf7103d7a7e88ed7
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-01-18 10:42:02 +0100

    Update Tests.java

--- src/test/java/org/capturecoop/cclogger/Tests.java
+++ src/test/java/org/capturecoop/cclogger/Tests.java
@@ -10,27 +10,17 @@ public class Tests {
     public void test() throws InterruptedException {
         CCLogger.log("Hello world!", LogLevel.INFO);
         CCLogger.log("There is no log file yet! Setting one now...", LogLevel.INFO);
-        CCLogger.log("Logging a stacktrace", LogLevel.WARNING);
-        gaga();
         CCLogger.setLogFile(new File("testlog.log"));
         CCLogger.log("Thats done!", LogLevel.INFO);
         CCLogger.log("Now we will enable the debug console!", LogLevel.INFO);
         CCLogger.enableDebugConsole(true);
-        Thread.sleep(5000);
-        CCLogger.log("Great! Disabling now", LogLevel.ERROR);
-        CCLogger.enableDebugConsole(false);
-        CCLogger.log("Waiting 2 seconds and enabling again...", LogLevel.ERROR);
-        Thread.sleep(2000);
-        CCLogger.log("Psssht. Secret debug message!", LogLevel.DEBUG);
-        CCLogger.enableDebugConsole(true);
-        Thread.sleep(5000);
-    }
-
-    public void gaga() {
-        gugu();
-    }
-
-    public void gugu() {
-        CCLogger.logStacktrace(new IOException("test"), LogLevel.WARNING);
+        CCLogger.log("Test", LogLevel.INFO);
+        Thread.sleep(1000);
+        CCLogger.log("BLABLA", LogLevel.INFO);
+        for(int i = 0; i < 40; i++) {
+            CCLogger.log("A: " + i, LogLevel.INFO);
+            Thread.sleep(100);
+        }
+        Thread.sleep(10000);
     }
 }
